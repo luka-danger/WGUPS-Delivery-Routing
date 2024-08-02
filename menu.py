9a99c42f511a3e4fedf86eed737e46e2eb2f2fb6
luka-danger <lukathevegan@gmail.com>
2024-08-02 17:02:50 -0600

    Add menu UI

---
+++ menu.py
@@ -0,0 +1,11 @@
+menu = ('''
+=======================================================
+          
+1. View All Delivery Information and Total Mileage
+2. View All Information of a Single Package at a Given Time
+3. View All Information of All Pakcages at a Given Time
+4. Exit Program
+
+========================================================
+          '''
+    )
