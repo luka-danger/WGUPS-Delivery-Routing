a2e70fb8a60bf1bc928c1b7a06bf5888e429a520
luka-danger <lukathevegan@gmail.com>
2024-08-06 14:11:29 -0600

    Fix typo
    Pakcage should be package

--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@ menu = ('''
           
 1. View All Delivery Information and Total Mileage
 2. View All Information of a Single Package at a Given Time
-3. View All Information of All Pakcages at a Given Time
+3. View All Information of All Packages at a Given Time
 4. Exit Program
 
 ========================================================
